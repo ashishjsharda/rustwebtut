20fcfbecf77eb57dcc78cabb9acb1fa64ef0d87a
Ashish Sharda <ashishjsharda@gmail.com>
2023-02-12 19:05:50 -0600

    add index implmentation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder, get};
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
+
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind("127.0.0.1:8081")?
+        .run().await
+}
+
